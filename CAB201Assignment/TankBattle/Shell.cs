950aed769c49db2b6076b244018c6f7ccc279158
seanocca <32288543+seanocca@users.noreply.github.com>
2017-10-02 22:11:30 +0100

    Debugging for tick
    Debugging code for Tick in Shell.cs

--- CAB201Assignment/TankBattle/Shell.cs
+++ CAB201Assignment/TankBattle/Shell.cs
@@ -16,8 +16,7 @@ namespace TankBattle
         private float y_velocity;
         private Blast explosion;
         private Opponent player;
-
-        protected Gameplay game;
+        
         private int wind;
 
         public Shell(float x, float y, float angle, float power, float gravity, Blast explosion, Opponent player)
@@ -37,25 +36,42 @@ namespace TankBattle
 
         public override void Tick()
         {
-            x = x + x_velocity;
-            y = y + y_velocity;
-
-            wind = game.GetWindSpeed();
-            x = x + (wind * 1000.0f);
-
-            if (x > Map.WIDTH || x < 0 || y > Map.HEIGHT || y < 0)
+            for (int i = 0; i < 10; i++)
             {
-                game.RemoveEffect(this);
-            }
+                Console.WriteLine("Started Loop");
+                x = x + x_velocity;
+                Console.WriteLine("x = {0}, x_vel = {1}", x, x_velocity);
+                y = y + y_velocity;
+                Console.WriteLine("y = {0}, y_vel = {1}", y, y_velocity);
 
-            if (game.CheckHitTank(x,y) == true)
-            {
-                player.HitPos(x, y);
-                explosion.Ignite(x, y);
-                game.AddWeaponEffect(explosion);
-                game.RemoveEffect(this);
+                wind = game.GetWindSpeed();
+
+                x = x + (wind / 1000.0f);
+
+                Console.WriteLine("Got Wind");
+
+
+                if (x >= Map.WIDTH || x <= 0 || y >= Map.HEIGHT || y <= 0)
+                {
+                    Console.WriteLine("Outside Bounds");
+                    game.RemoveEffect(this);
+                    return;
+                }
+
+                if (game.CheckHitTank(x, y) == true)
+                {
+                    Console.WriteLine("TankHit true");
+                    player.HitPos(x, y);
+                    Console.WriteLine("HitPos working");
+                    explosion.Ignite(x, y);
+                    Console.WriteLine("Ignite Working");
+                    game.AddWeaponEffect(explosion);
+                    Console.WriteLine("AddWeaponEffect Working");
+                    game.RemoveEffect(this);
+                    Console.WriteLine("RemoveEffect Working");
+                }
+                y = y + gravity;
             }
-            y = y + gravity;
         }
 
         public override void Display(Graphics graphics, Size size)
