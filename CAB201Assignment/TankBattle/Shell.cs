149455fed6ae11f8e5cd5feaf91506902d3e02b4
seanocca <32288543+seanocca@users.noreply.github.com>
2017-10-02 22:25:05 +0100

    Got Tick working
    Tick in Shell.cs working

--- CAB201Assignment/TankBattle/Shell.cs
+++ CAB201Assignment/TankBattle/Shell.cs
@@ -31,6 +31,8 @@ namespace TankBattle
             x_velocity = (float)Math.Cos(angleRadians) * magnitude;
             y_velocity = (float)Math.Sin(angleRadians) * -magnitude;
 
+            this.explosion = explosion;
+
             this.player = player;
         }
 
@@ -38,39 +40,27 @@ namespace TankBattle
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Started Loop");
-                x = x + x_velocity;
-                Console.WriteLine("x = {0}, x_vel = {1}", x, x_velocity);
-                y = y + y_velocity;
-                Console.WriteLine("y = {0}, y_vel = {1}", y, y_velocity);
+                x += x_velocity;
+                y += y_velocity;
 
                 wind = game.GetWindSpeed();
 
-                x = x + (wind / 1000.0f);
-
-                Console.WriteLine("Got Wind");
-
+                x += (wind / 1000.0f);
 
                 if (x >= Map.WIDTH || x <= 0 || y >= Map.HEIGHT || y <= 0)
                 {
-                    Console.WriteLine("Outside Bounds");
                     game.RemoveEffect(this);
                     return;
                 }
 
                 if (game.CheckHitTank(x, y) == true)
                 {
-                    Console.WriteLine("TankHit true");
                     player.HitPos(x, y);
-                    Console.WriteLine("HitPos working");
                     explosion.Ignite(x, y);
-                    Console.WriteLine("Ignite Working");
                     game.AddWeaponEffect(explosion);
-                    Console.WriteLine("AddWeaponEffect Working");
                     game.RemoveEffect(this);
-                    Console.WriteLine("RemoveEffect Working");
                 }
-                y = y + gravity;
+                y += gravity;
             }
         }
 
